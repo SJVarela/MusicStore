28381d27c7748e807cfbc1a921fd246db216e5cf
Santa <Santa@SANTA-PC>
2017-06-21 19:10:32 -0300

    Easy tutorial project

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View(new List<Genre>
+            {
+                new Genre {Name = "Disco" },
+                new Genre {Name = "Jazz" },
+                new Genre {Name = "Rock" }
+            });
+        }
+        public ActionResult Browse(string genre)
+        {
+            return View(new Genre { Name = genre});
+        }
+        public ActionResult Details(int id)
+        {
+            return View(new Album { Title = "Album "+ id});
+        }
+    }
+}
